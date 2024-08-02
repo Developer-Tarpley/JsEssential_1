b8192ffd13972353d9e52c8b705037dca6cd5645
Corey Tarpley <developertarpley@gmail.com>
2024-08-01 23:29:49 -0400

    created delete functionality

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -1,4 +1,5 @@
 const books = [];
+let isEmptyBooks = false;
 function addBook() {
     const bookname = document.getElementById('bookname').value;
     const authorname = document.getElementById('authorname').value;
@@ -13,7 +14,7 @@ function addBook() {
             pagesNumber, pagesNumber
         };
         books.push(book);
-        console.log(books)
+        // console.log(books)
         showbooks();
         clearInputs();
     } else {
@@ -22,23 +23,50 @@ function addBook() {
 };
 
 function showbooks() {
-    const booksDiv = books.map((book, index) => {
-        return (
-            `
-            <h1>book Number: ${index + 1}</h1>
-            <p><strong>Book Name: </strong>${book.name}</p>
-            <p><strong>Author Name: </strong>${book.authorname}</p>
-            <p><strong>Book Description: </strong>${book.bookDescription}</p>
-            <p><strong>No. of Pages: </strong>${book.pagesNumber}</p>
-            `
-        )
-    });
-    document.getElementById('books').innerHTML = booksDiv.join('')
-};
+    if (books.length === 0) {
+        const nobooks = `There are no books available.`
+        const h3 = document.createElement('h3');
+        h3.innerText = nobooks
+        console.log('no books!', nobooks)
+        document.getElementById('books').innerHTML = '';
+        document.getElementById('books').appendChild(h3);
+    
+    }else{
 
-function clearInputs() {
-    document.getElementById('bookname').value = '';
-    document.getElementById('authorname').value = '';
-    document.getElementById('bookDescription').value = '';
-    document.getElementById('pagesNumber').value = '';
-};
+        const booksDiv = books.map((book, index) => {
+            return (
+                `
+                <h1>book Number: ${index + 1}</h1>
+                <p><strong>Book Name: </strong>${book.name}</p>
+                <p><strong>Author Name: </strong>${book.authorname}</p>
+                <p><strong>Book Description: </strong>${book.bookDescription}</p>
+                <p><strong>No. of Pages: </strong>${book.pagesNumber}</p>
+                <button onclick="return deleteBook(${index})">Delete</button>
+                <button onclick="return editBookDetails(${index})">Edit</button>
+                `
+            )
+        });
+        document.getElementById('books').innerHTML = booksDiv.join('');
+    }
+}
+
+    function clearInputs() {
+        document.getElementById('bookname').value = '';
+        document.getElementById('authorname').value = '';
+        document.getElementById('bookDescription').value = '';
+        document.getElementById('pagesNumber').value = '';
+    };
+
+    function deleteBook(index) {
+        if (confirm('Are you sure you want to delete book?')) {
+            books.splice(index, 1);
+            alert('Book has successfully been deleted');
+
+            showbooks();
+            console.log('deletes: ', books)
+        }
+    }
+
+    function editBookDetails(){
+        console.log("edits")
+    }
